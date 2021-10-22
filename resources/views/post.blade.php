3d63eddcf1c9e7f1495fec8a4e087a21a2ec332c
Scarnface <scarnface@tutanota.com>
2021-10-22 08:42:12 +0100

    Updated to use layouts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
+<x-layout>
 
-<body>
   <article>
 
-    <h1><?= $post->title; ?></h1>
+    <h1>{{ $post->title; }}</h1>
 
-    <div><?= $post->body; ?></div>
+    <div>{!! $post->body; !!}</div>
 
     <a href="/">Go Back</a>
 
   </article>
-</body>
+
+</x-layout>
