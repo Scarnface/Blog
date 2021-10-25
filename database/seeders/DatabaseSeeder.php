509971dc23b2de987493fbc0421a3741b5c70e14
Scarnface <scarnface@tutanota.com>
2021-10-25 09:40:40 +0100

    Factories added for auto DB seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,43 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-          'name' => 'Personal',
-          'slug' => 'personal'
+        $user = User::factory()->create([
+          'name' => 'Scarnface'
         ]);
 
-        $family = Category::create([
-          'name' => 'Family',
-          'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-          'name' => 'Work',
-          'slug' => 'work'
-        ]);
-
-        Post::create([
-          'user_id' => $user->id,
-          'category_id' => $family->id,
-          'title' => 'My Family Post',
-          'slug' => 'my-first-post',
-          'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-          'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>\r\n \r\n<p>Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet.</p>'
-        ]);
-
-        Post::create([
-          'user_id' => $user->id,
-          'category_id' => $work->id,
-          'title' => 'My Work Post',
-          'slug' => 'my-second-post',
-          'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-          'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>\r\n \r\n<p>Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet.</p>'
+        Post::factory(5)->create([
+          'user_id' => $user->id
         ]);
     }
 }
