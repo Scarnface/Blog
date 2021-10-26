b265ee67e100615720ae814c6a9a35196937ae58
Scarnface <scarnface@tutanota.com>
2021-10-26 14:43:53 +0100

    Search added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Facade\Ignition\QueryRecorder\Query;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,14 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'excerpt', 'body'];
+
+    public function scopeFilter($query, array $filters)
+    {
+      $query->when($filters['search'] ?? false, fn ($query, $search) => 
+        $query
+          ->where('title', 'like', '%' . $search . '%')
+          ->orWhere('body', 'like', '%' . $search . '%'));
+    }
 
     public function category()
     {
