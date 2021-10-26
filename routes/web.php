51a691f2709c516c7fb3bc14647936846081cca2
Scarnface <scarnface@tutanota.com>
2021-10-26 10:25:25 +0100

    Named routes and SVG icon component added

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
     'categories' => Category::all()
   ]);
 
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
   
@@ -41,7 +41,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
     'categories' => Category::all()
   ]);
 
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     
