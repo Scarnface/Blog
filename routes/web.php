04f8af5e9b32a2febf574136ba2ddb3f3731ce20
Scarnface <scarnface@tutanota.com>
2021-10-26 08:40:34 +0100

    Dynamic drop down menu

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
   return view('posts', [
-    'posts' => Post::latest()->with('category', 'author')->get()
+    'posts' => Post::latest()->with('category', 'author')->get(),
+    'categories' => Category::all()
   ]);
 
 });
@@ -35,7 +36,9 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     
   return view('posts', [
-    'posts' => $category->posts->load(['category', 'author'])
+    'posts' => $category->posts->load(['category', 'author']),
+    'currentCategory' => $category,
+    'categories' => Category::all()
   ]);
 
 });
@@ -43,7 +46,8 @@ Route::get('categories/{category:slug}', function (Category $category) {
 Route::get('authors/{author:username}', function (User $author) {
     
   return view('posts', [
-    'posts' => $author->posts->load(['category', 'author'])
+    'posts' => $author->posts->load(['category', 'author']),
+    'categories' => Category::all()
   ]);
 
 });
