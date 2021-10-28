e516e8dbc63f1ad1fe967596ee80fe7fb040d5a9
Scarnface <scarnface@tutanota.com>
2021-10-28 15:32:44 +0100

    Session regenerate at login

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,6 +20,7 @@ class SessionsController extends Controller
 
       if (Auth::attempt($attributes))
       {
+        session()->regenerate();
         return redirect('/')->with('success', 'Welcome!');
       }
 
