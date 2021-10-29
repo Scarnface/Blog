f85fe525ee67ba6eb8a339b029efc86a536ed479
Scarnface <scarnface@tutanota.com>
2021-10-29 12:01:12 +0100

    Comments made dynamic. Factory setup

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,17 +1,14 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 rounded-xl p-6 space-x-4">
   <div class="flex-shrink-0">
-    <img src="https://i.pravatar.cc/60" alt="User Avatar" width="60" height="60" class="rounded-xl" />
+    <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="User Avatar" width="60" height="60" class="rounded-xl" />
   </div>
   <div>
     <header class="mb-4">
-      <h3 class="font-bold">Scarnface</h3>
-      <p class="text-xs">Posted <time>8 months ago</time></p>
+      <h3 class="font-bold">{{ $comment->author->username }}</h3>
+      <p class="text-xs">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></p>
     </header>
-    <p>molestiae-incidunt-recusandae-pariatur-consectetur-iste-nemo-incidunt,Veniam quod sed sapiente ex harum 
-      et dolore. Dolor rerum vel vitae reprehenderit occaecati at. Deserunt quo explicabo sint vel. Vel aut in fugit
-      nobis earum numquam voluptatem quam. Nulla quia eum omnis et soluta voluptatem officiis. Quo voluptate non illum 
-      neque neque eos adipisci quidem. Explicabo rerum quas sunt explicabo laudantium omnis. Voluptatem eos modi ipsam 
-      a voluptas culpa. Impedit dolor at adipisci assumenda.
-    </p>                
+    <p>{{ $comment->body }}</p>                
   </div>
 </article>
