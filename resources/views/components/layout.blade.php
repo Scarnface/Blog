fa6646d0634bcb5c80da0ed9f80c6304121807f2
Scarnface <scarnface@tutanota.com>
2021-10-31 14:49:25 +0000

    Newsletter linking.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -31,7 +31,7 @@
           <a href="{{ url('/register') }}" class="text-xs font-bold uppercase ml-3">Register</a>
         @endauth
 
-        <a href="#"
+        <a href="#newsletter"
            class="bg-blue-500 hover:bg-blue-600 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
           Subscribe for Updates
         </a>
@@ -40,7 +40,7 @@
 
     {{ $slot }}
 
-    <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+    <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
       <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
       <h5 class="text-3xl">Stay in touch with the latest posts</h5>
       <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
