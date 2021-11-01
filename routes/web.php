1c91b324e51ea695e7c52d38362c9f4af214e507
Scarnface <scarnface@tutanota.com>
2021-11-01 11:07:13 +0000

    Admin status added.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
+
 Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
@@ -22,4 +24,5 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('sessions', [SessionsController::class, 'store'])->middleware('guest');
+
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
