cf517a023c04eaffd6fa72259fb942ae6f2b0f48
Scarnface <scarnface@tutanota.com>
2021-11-01 08:09:43 +0000

    Chimpmail integration.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,12 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
+<style>
+  html {
+    scroll-behavior: smooth;
+  }
+</style>
+
 <body style="font-family: Open Sans, sans-serif">
   <section class="px-6 py-8">
     <nav class="md:flex md:justify-between md:items-center">
@@ -48,14 +54,25 @@
       <div class="mt-10">
         <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
 
-          <form method="POST" action="#" class="lg:flex text-sm">
+          <form method="POST" action="{{ url('/newsletter') }}" class="lg:flex text-sm">
+            @csrf
+
             <div class="lg:py-3 lg:px-5 flex items-center">
               <label for="email" class="hidden lg:inline-block">
                 <img src="/images/mailbox-icon.svg" alt="mailbox letter">
               </label>
 
-              <input id="email" type="text" placeholder="Your email address"
-                     class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+              <div>
+                <input id="email"
+                        name="email"
+                        type="text"
+                        placeholder="Your email address"
+                        class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+
+                @error('email')
+                  <span class="text-xs text-red-500">{{ $message }}</span>
+                @enderror
+              </div>
             </div>
 
             <button type="submit"
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\PostCommentsController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\SessionsController;
-use Illuminate\Support\Facades\Route;
+use App\{
+  Http\Controllers\PostController,
+  Http\Controllers\PostCommentsController,
+  Http\Controllers\RegisterController,
+  Http\Controllers\SessionsController
+};
+use Illuminate\{
+  Support\Facades\Route,
+  Validation\ValidationException
+};
+use MailchimpMarketing\ApiClient;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
@@ -17,3 +23,28 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('sessions', [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+Route::post('newsletter', function () {
+  request()->validate(['email' => ['required', 'email']]);
+
+  $mailchimp = new ApiClient();
+
+  $mailchimp->setConfig([
+    'apiKey' => config('services.mailchimp.key'),
+    'server' => 'us5'
+  ]);
+
+  try {
+    $mailchimp->lists->addListMember("2036167f13", [
+      "email_address" => request('email'),
+      "status" => 'subscribed',
+    ]);
+  } catch (Exception $e) {
+    throw ValidationException::withMessages([
+      'email' => 'This email could not be added to the list.'
+    ]);
+  }
+
+  return redirect('/')
+    ->with('success', 'Subscribed!');
+});
